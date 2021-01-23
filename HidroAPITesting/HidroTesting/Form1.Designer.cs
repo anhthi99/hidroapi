c275850b8393688edc73ea1271cc426e6a1e8e3c
Anh Thi <thi12171999@gmail.com>
2021-01-23 20:48:57 +0700

    add icon for app

--- HidroAPITesting/HidroTesting/Form1.Designer.cs
+++ HidroAPITesting/HidroTesting/Form1.Designer.cs
@@ -29,10 +29,21 @@ namespace HidroTesting
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(751, 605);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "API Testing";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
